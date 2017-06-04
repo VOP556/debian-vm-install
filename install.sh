ea11964b88e6a07680e2a6eed13c7c941142e355
VOP556 <hellraiser@burningmountain.de>
2017-06-04 17:34:13 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,69 @@
+#!/bin/sh -e
+ 
+# A script to create debian VM as a KVM guest using virt-install in fully
+# automated way based on preseed.cfg
+
+# Domain is necessary in order to avoid debian installer to
+# require manual domain entry during the install.
+DOMAIN=`/bin/hostname -d` # Use domain of the host system
+#DOMAIN="dp-net.com" # Alternatively, hardcode domain
+# NB: See postinst.sh for ability to override domain received from
+# DHCP during the install.
+
+#DIST_URL="http://ftp.de.debian.org/debian/dists/stretch/main/installer-amd64/"
+DIST_URL='http://ftp.de.debian.org/debian/dists/stretch/main/installer-amd64/'
+LINUX_VARIANT="debian9"
+# NB: Also see preseed.cfg for debian mirror hostname.
+
+if [ $# -lt 1 ]
+then
+	cat <<EOF
+Usage: $0 <GUEST_NAME> [MAC_ADDRESS]"
+
+  GUEST_NAME    used as guest hostname, name of the VM and image file name
+  MAC_ADDRESS   allows to use specific MAC on the network, this is helpful
+                when DHCP server expects your guest to have predefined MAC
+
+Examples:
+
+  $0 backend 52:54:00:bf:b3:86 # create guest named "backend" with given MAC
+
+  $0 wow # create guest named "wow" with random MAC
+EOF
+	exit 1
+fi
+
+MAC="RANDOM"
+if [ $# -eq 2 ]
+then
+	MAC=$2
+fi
+
+# Fetch SSH key from github.
+#wget -q https://github.com/pin.keys -O postinst/authorized_keys
+
+# Create tarball with some stuff we would like to install into the system.
+#tar cvfz postinst.tar.gz postinst
+
+#create lvm partition
+lvcreate --name ${1} --size 10G ssd
+ 
+virt-install \
+--connect=qemu:///system \
+--name=${1} \
+--memory=2048 \
+--vcpus=1 \
+--cpu host \
+--disk /dev/ssd/${1},bus=virtio \
+--initrd-inject=preseed.cfg \
+--initrd-inject=postinst.sh \
+--location ${DIST_URL} \
+--os-type linux \
+--os-variant ${LINUX_VARIANT} \
+--virt-type=kvm \
+--controller usb,model=none \
+--graphics none \
+--network bridge=br0,mac=${MAC},model=virtio \
+--extra-args="auto=true hostname="${1}" domain="${DOMAIN}" console=tty0 console=ttyS0,115200n8 serial"
+
+#rm postinst.tar.gz
---
+++ postinst.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# This script is run by debian installer using preseed/late_command
+# directive, see preseed.cfg
+
+# Setup console, remove timeout on boot.
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="console=ttyS0"/g; s/TIMEOUT=5/TIMEOUT=0/g' /etc/default/grub
+update-grub
+
+# Members of `sudo` group are not asked for password.
+#sed -i 's/%sudo\tALL=(ALL:ALL) ALL/%sudo\tALL=(ALL:ALL) NOPASSWD:ALL/g' /etc/sudoers
+
+# Empty message of the day.
+echo -n > /etc/motd
+
+# Unpack postinst tarball.
+#tar -x -v -z -C/tmp -f /tmp/postinst.tar.gz
+
+# Install SSH key for pin.
+#mkdir -m700 /home/pin/.ssh
+#cat /tmp/postinst/authorized_keys > /home/pin/.ssh/authorized_keys
+#chown -R pin:pin /home/pin/.ssh
+
+# Install collectd and config.
+#apt-get install -y collectd-core
+#cp /tmp/postinst/collectd.conf /etc/collectd/
+
+# Remove some non-essential packages.
+#DEBIAN_FRONTEND=noninteractive apt-get purge -y laptop-detect tasksel dictionaries-common emacsen-common iamerican ibritish ienglish-common ispell
+
+# Set domain name in hosts file
+#sed -i 's/127.0.1.1\t\([a-z]*\).*/127.0.1.1\t\1\.dp\-net\.com\t\1/' /etc/hosts
+
+# Avoid using DHCP-server provided domain name.
+#sed -i 's/#supersede.*/supersede domain-name "dp-net.com";/' /etc/dhcp/dhclient.conf
+
